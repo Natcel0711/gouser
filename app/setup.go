fd81fb52ee9b3ba1999aee4940c67a2832459ccb
Natcel0711 <natcelnieves@gmail.com>
2023-01-26 11:40:24 -0400

    changed env

--- app/setup.go
+++ app/setup.go
@@ -3,20 +3,15 @@ package app
 import (
 	"errors"
 
-	"github.com/Natcel0711/gouser/config"
 	"github.com/Natcel0711/gouser/middlewares"
 	"github.com/Natcel0711/gouser/router"
 	"github.com/go-chi/chi/v5"
 )
 
 func SetupApp() error {
-	err := config.LoadENV()
-	if err != nil {
-		return err
-	}
 	r := chi.NewRouter()
 	middlewares.UseMiddlewares(r)
-	err = router.SetupRouters(r)
+	err := router.SetupRouters(r)
 	if err != nil {
 		return errors.New("error setting up router")
 	}
